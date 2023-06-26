fc7cf467f52cbac38e21d62b7e41977de1997d30
jimmylizheng <jimmyli@umich.edu>
2023-06-26 17:32:47 +0000

    sync code

--- point_e/examples/loading_latency_eval.py
+++ point_e/examples/loading_latency_eval.py
@@ -54,36 +54,113 @@ def main():
     
     # print(f"bp0{get_gpu_memory_usage()}")
     print(f"b{-init_t+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
-
-    t_st=time.time()
-    g_st=get_gpu_memory_usage()
-    upsampler_model = model_from_config(MODEL_CONFIGS['upsample'], device)
-    print(f"up_config:{-t_st+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
     
     t_st=time.time()
     g_st=get_gpu_memory_usage()
-    upsampler_model.eval()
-    print(f"up_eval:{-t_st+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
+    base_name1 = 'base300M' # use base300M or base1B for better results
+    base_model1 = model_from_config(MODEL_CONFIGS[base_name1], device)
+    print(f"base_config:{-t_st+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
+    
     
     t_st=time.time()
     g_st=get_gpu_memory_usage()
-    upsampler_diffusion = diffusion_from_config(DIFFUSION_CONFIGS['upsample'])
-    print(f"up_diff_config:{-t_st+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
-
-    # print(f"b1p{get_gpu_memory_usage()}")
-
-    # base_model.load_state_dict(load_checkpoint(base_name, device))
+    base_model1.eval()
+    print(f"base_eval:{-t_st+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
     
-    # print(f"b2p{get_gpu_memory_usage()}")
+    t_st=time.time()
+    g_st=get_gpu_memory_usage()
+    base_diffusion1 = diffusion_from_config(DIFFUSION_CONFIGS[base_name1])
+    print(f"base_diff_config:{-t_st+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
+    # print(f"bp{get_gpu_memory_usage()}")
+    
+    t_st=time.time()
+    g_st=get_gpu_memory_usage()
+    base_model1.load_state_dict(load_checkpoint(base_name, device))
+    print(f"base_dict:{-t_st+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
+    
+    # print(f"bp0{get_gpu_memory_usage()}")
+    print(f"1b{-init_t+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
 
     t_st=time.time()
     g_st=get_gpu_memory_usage()
-    upsampler_model.load_state_dict(load_checkpoint('upsample', device))
-    print(f"up_dict:{-t_st+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
+    base_name2 = 'base300M' # use base300M or base1B for better results
+    base_model2 = model_from_config(MODEL_CONFIGS[base_name2], device)
+    print(f"base_config:{-t_st+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
     
-    # print(f"e{get_gpu_memory_usage()}")
-    # print(f"e{-init_t+time.time()}")
-    print(f"{-init_t+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
+    t_st=time.time()
+    g_st=get_gpu_memory_usage()
+    base_model2.eval()
+    print(f"base_eval:{-t_st+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
+    
+    t_st=time.time()
+    g_st=get_gpu_memory_usage()
+    base_diffusion2 = diffusion_from_config(DIFFUSION_CONFIGS[base_name])
+    print(f"base_diff_config:{-t_st+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
+    # print(f"bp{get_gpu_memory_usage()}")
+    
+    t_st=time.time()
+    g_st=get_gpu_memory_usage()
+    base_model2.load_state_dict(load_checkpoint(base_name2, device))
+    print(f"base_dict:{-t_st+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
+    
+    # print(f"bp0{get_gpu_memory_usage()}")
+    print(f"2b{-init_t+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
+    
+    t_st=time.time()
+    g_st=get_gpu_memory_usage()
+    base_name3 = 'base300M' # use base300M or base1B for better results
+    base_model3 = model_from_config(MODEL_CONFIGS[base_name3], device)
+    print(f"base_config:{-t_st+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
+    
+    t_st=time.time()
+    g_st=get_gpu_memory_usage()
+    base_model3.eval()
+    print(f"base_eval:{-t_st+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
+    
+    t_st=time.time()
+    g_st=get_gpu_memory_usage()
+    base_diffusion3 = diffusion_from_config(DIFFUSION_CONFIGS[base_name3])
+    print(f"base_diff_config:{-t_st+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
+    # print(f"bp{get_gpu_memory_usage()}")
+    
+    t_st=time.time()
+    g_st=get_gpu_memory_usage()
+    base_model3.load_state_dict(load_checkpoint(base_name3, device))
+    print(f"base_dict:{-t_st+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
+    
+    # print(f"bp0{get_gpu_memory_usage()}")
+    print(f"3b{-init_t+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
+    
+
+    # t_st=time.time()
+    # g_st=get_gpu_memory_usage()
+    # upsampler_model = model_from_config(MODEL_CONFIGS['upsample'], device)
+    # print(f"up_config:{-t_st+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
+    
+    # t_st=time.time()
+    # g_st=get_gpu_memory_usage()
+    # upsampler_model.eval()
+    # print(f"up_eval:{-t_st+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
+    
+    # t_st=time.time()
+    # g_st=get_gpu_memory_usage()
+    # upsampler_diffusion = diffusion_from_config(DIFFUSION_CONFIGS['upsample'])
+    # print(f"up_diff_config:{-t_st+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
+
+    # # print(f"b1p{get_gpu_memory_usage()}")
+
+    # # base_model.load_state_dict(load_checkpoint(base_name, device))
+    
+    # # print(f"b2p{get_gpu_memory_usage()}")
+
+    # t_st=time.time()
+    # g_st=get_gpu_memory_usage()
+    # upsampler_model.load_state_dict(load_checkpoint('upsample', device))
+    # print(f"up_dict:{-t_st+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
+    
+    # # print(f"e{get_gpu_memory_usage()}")
+    # # print(f"e{-init_t+time.time()}")
+    # print(f"{-init_t+time.time()} seconds, gpu: {get_gpu_memory_usage()-g_st}")
     
     # t_st=time.time()
     # g_st=get_gpu_memory_usage()
